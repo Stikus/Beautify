d4533e59e79d5156c7ddbf31549a5554a7b39cb7
Konstantin Grammatikati <grammatikati@cspmz.ru>
2019-05-23 15:09:25 +0300

    Update test

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -62,9 +62,6 @@ wget -q "ftp://bioftp.cspmz.ru/certs/cspmzCA.pem" -O "/etc/ssl/certs/cspmzCA.pem
 # Add user 'bio' to 'docker' group
 usermod -a -G docker bio
 
-# Add GKS vm01 pub RSA-key
-mkdir -p "$HOME/.ssh" && wget -q "ftp://bioftp.cspmz.ru/certs/keys/GKS_id_rsa.pub" -O ->> "$HOME/.ssh/authorized_keys"
-
 # python3.6 & pip3
 apt-get --yes --no-install-recommends install python3.6 python3.6-dev python3-pip \
     && rm /usr/bin/python3 \
@@ -72,6 +69,38 @@ apt-get --yes --no-install-recommends install python3.6 python3.6-dev python3-pi
     && ln -s /usr/bin/python3.6 /usr/bin/python
 pip3 install --upgrade pip
 
-wget -q https://raw.githubusercontent.com/Stikus/Beautify/master/ubuntu_init_2.sh
-bash ubuntu_init_2.sh # for pip update - second part
-exit # from 'root'
+hash -d pip3
+
+# wget -q https://raw.githubusercontent.com/Stikus/Beautify/master/ubuntu_init_2.sh
+# bash ubuntu_init_2.sh # for pip update - second part
+
+pip3 install --upgrade wheel setuptools
+pip3 install --upgrade psutil
+
+# java8
+apt-get --yes --no-install-recommends install openjdk-8-jre
+export _JAVA_OPTIONS="-Djava.io.tmpdir=$TMPDIR"
+
+# cwltool 1.0.20190228155703
+pip3 install 'cwltool==1.0.20190228155703'
+
+# shellcheck 0.6.0
+wget -q "https://shellcheck.storage.googleapis.com/shellcheck-v0.6.0.linux.x86_64.tar.xz" -O "shellcheck-v0.6.0.linux.x86_64.tar.xz" \
+    && tar -xJf "shellcheck-v0.6.0.linux.x86_64.tar.xz" \
+    && mv shellcheck-v0.6.0/shellcheck /usr/local/bin \
+    && rm "shellcheck-v0.6.0.linux.x86_64.tar.xz" \
+    && rm -r shellcheck-v0.6.0/
+
+# memUsage (both python 2 & 3) (Olga)
+# psutil >= 2.2.1 (Tested with 5.6.1 - ok; 1.2.1 - err) - additional python package required for memUsage.
+wget -q "https://raw.githubusercontent.com/ozolotareva/housekeeping-scr/master/memUsage.py" -O - | tr -d '\r' > "/usr/local/bin/memUsage.py" \
+    && sed -i -e '1i#!/usr/bin/env python' "/usr/local/bin/memUsage.py" \
+    && chmod +x "/usr/local/bin/memUsage.py"
+export MEMUSAGE="/usr/local/bin/memUsage.py"
+
+
+export SOFT="/soft"
+mkdir -p "$SOFT"
+
+# Add GKS vm01 pub RSA-key
+su bio && mkdir -p "$HOME/.ssh" && wget -q "ftp://bioftp.cspmz.ru/certs/keys/GKS_id_rsa.pub" -O ->> "$HOME/.ssh/authorized_keys"
